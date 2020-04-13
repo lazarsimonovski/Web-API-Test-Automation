c49c3f540080e83fc2046bbe1b24fe15720b1a18
Ivana <ivanastamardjioska@gmail.com>
2020-04-13 18:36:22 +0200

    some changes in first test

--- src/test/java/Get200.java
+++ src/test/java/Get200.java
@@ -1,10 +1,18 @@
+import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.testng.annotations.Test;
+
+import java.io.IOException;
 
 public class Get200 {
 
+    public static final String BASE_ENDPOINT = "https://api.github.com";
+    HttpClient client = HttpClientBuilder.create().build();
+
     @Test
-    public  void baseUrlReturns200(){
-        HttpGet get = new HttpGet("https://api.github.com");
+    public void baseUrlReturnes200() throws IOException {
+        HttpGet get = new HttpGet(BASE_ENDPOINT);
+        client.execute(get);
     }
 }
