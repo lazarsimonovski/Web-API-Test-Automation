52ba98a006b5e4422837d752302e0586827be949
Ivana <ivanastamardjioska@gmail.com>
2020-04-13 20:05:33 +0200

    add 3 tests

--- src/test/java/Get200.java
+++ src/test/java/Get200.java
@@ -1,8 +1,11 @@
 import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
@@ -10,12 +13,44 @@ import java.io.IOException;
 public class Get200 {
 
     public static final String BASE_ENDPOINT = "https://api.github.com";
-    HttpClient client = HttpClientBuilder.create().build();
+    CloseableHttpClient client;
+    CloseableHttpResponse response;
+
+    @BeforeMethod
+    public void setup(){
+        client = HttpClientBuilder.create().build();
+    }
+
+    @AfterMethod
+    public void closeResources() throws IOException {
+        client.close();
+        response.close();
+    }
 
     @Test
     public void baseUrlReturnes200() throws IOException {
         HttpGet get = new HttpGet(BASE_ENDPOINT);
-        HttpResponse response = client.execute(get);
+        response = client.execute(get);
+
+        int actualStatus = response.getStatusLine().getStatusCode();
+
+        Assert.assertEquals(actualStatus, 200);
+    }
+
+    @Test
+    public void rateLimitReturnes200() throws IOException {
+        HttpGet get = new HttpGet(BASE_ENDPOINT + "/rate_limit");
+        response = client.execute(get);
+
+        int actualStatus = response.getStatusLine().getStatusCode();
+
+        Assert.assertEquals(actualStatus, 200);
+    }
+
+    @Test
+    public void searchReposReturnes200() throws IOException {
+        HttpGet get = new HttpGet(BASE_ENDPOINT + "/search/repositories?q=java");
+        response = client.execute(get);
 
         int actualStatus = response.getStatusLine().getStatusCode();
 
