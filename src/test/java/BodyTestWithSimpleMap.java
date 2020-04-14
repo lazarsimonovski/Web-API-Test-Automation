0ad88678a024148cb9c667368487ca607c410936
Ivana <ivanastamardjioska@gmail.com>
2020-04-14 15:41:48 +0200

    add bodytest

---
+++ src/test/java/BodyTestWithSimpleMap.java
@@ -0,0 +1,19 @@
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.util.EntityUtils;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class BodyTestWithSimpleMap extends BaseClass{
+
+    @Test
+    public void returnesCorrectLogin() throws IOException {
+        HttpGet get = new HttpGet(BASE_ENDPOINT + "/users/lazarsimonovski");
+
+        response = client.execute(get);
+
+        String jsonBody = EntityUtils.toString(response.getEntity());
+
+        System.out.println(jsonBody);
+    }
+}
