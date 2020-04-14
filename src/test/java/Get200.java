4619aef6bf88f1dc162c2f34f0957009b39e9eb4
Ivana <ivanastamardjioska@gmail.com>
2020-04-14 14:13:06 +0200

    refactor

--- src/test/java/Get200.java
+++ src/test/java/Get200.java
@@ -1,11 +1,4 @@
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
@@ -13,20 +6,6 @@ import java.io.IOException;
 import static org.testng.Assert.assertEquals;
 
 public class Get200 extends BaseClass {
-
-    CloseableHttpClient client;
-    CloseableHttpResponse response;
-
-    @BeforeMethod
-    public void setup(){
-        client = HttpClientBuilder.create().build();
-    }
-
-    @AfterMethod
-    public void closeResources() throws IOException {
-        client.close();
-        response.close();
-    }
 
     @Test
     public void baseUrlReturnes200() throws IOException {
