b23a3a7d9168461e771be44ed2516a5694603176
Lazar Simonovski <lazarsimonovski@hotmail.com>
2020-04-16 00:37:06 +0200

    writing tests for delete and post

---
+++ src/main/java/Credentials.java
@@ -0,0 +1,8 @@
+public class Credentials {
+
+    public static String TOKEN = "enter-token";
+
+    public static String EMAIL = "enter-email";
+
+    public static String PASSWORD = "enter-password";
+}
